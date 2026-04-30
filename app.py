1afa4a5328ec66c22b3ba85cbc27a85d46b75096
itz-ema <attahema14@gmail.com>
2026-05-01 11:43:03 +1200

    Creating the app making the flask app

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, g, request, redirect
+import sqlite3
+DATABASE = "database.db"
+app = Flask(__name__)
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+app = Flask(__name__)
