60dd13a3d515eec4ed740568350190d7b6af2b51
itz-ema <attahema14@gmail.com>
2026-05-03 09:47:03 +1200

    Expense page (route and html)

--- app.py
+++ app.py
@@ -59,6 +59,43 @@ def delete_category(id):
     return redirect (url_for("view_categories"))
 
 
+@app.route("/view_expenses")
+def view_expenses():
+    sql = "SELECT expenses.id, expenses.name, expenses.amount_spent, category.name AS category FROM expenses JOIN category ON expenses.category_id = category.id"
+    expenses = query_db(sql)
+    sql = "SELECT * FROM category"
+    categories = query_db(sql)
+    return render_template("expenses.html", expenses=expenses, categories=categories)
+
+@app.route ("/add_expenses", methods = ["POST"])
+def add_expenses():
+    category_id = request.form['category_id']
+    expenses_name = request.form ['name']
+    amount_spent = request.form ['amount_spent']
+    sql = "INSERT INTO expenses (category_id, name, amount_spent) VALUES (?, ?, ?)"
+    query_db(sql,(category_id, expenses_name, amount_spent,))
+    get_db().commit()    
+    return redirect (url_for("view_expenses"))
+
+@app.route ("/edit_expenses/<int:id>", methods = ["POST"])
+def edit_expenses(id):
+    category_id = request.form['category_id']
+    expenses_name = request.form ['name']
+    amount_spent = request.form ['amount_spent']
+    sql = "UPDATE expenses SET category_id =?, name =?, amount_spent = ? WHERE id = ?"
+    query_db(sql,(category_id, expenses_name, amount_spent,id,))
+    get_db().commit()
+    return redirect (url_for("view_expenses"))
+    
+
+@app.route("/delete_expenses/<int:id>")
+def delete_expenses(id):
+    sql = "DELETE FROM expenses WHERE id =?"
+    query_db(sql,(id,))
+    get_db().commit()
+    return redirect (url_for("view_expenses"))
+
+
 
 
 if __name__ == "__main__":
